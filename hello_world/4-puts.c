2d198f51a13b2a3e993332693763a57feab5b540
fatima9821 <bahfatoumat9821@gmail.com>
2023-10-13 04:55:32 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-
+#include <stdio.h> 
 /**
- * main ("Programming is like building a multilingual puzzle, followed by a new line.")
- * return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle, followed by a new line.,\n");
-    return (0);
-}
+	
+puts("\"Programming is like building a multilingual puzzle");
 
+return (0);
+}
