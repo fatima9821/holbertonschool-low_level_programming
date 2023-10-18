8ada4264b28aff1772dab3ff1c6e2d41d7c2c433
fatima9821 <bahfatoumat9821@gmail.com>
2023-10-18 01:05:44 -0700

    6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -12,7 +13,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 10; i++)
 	putchar(i);
 	putchar('\n');
 
