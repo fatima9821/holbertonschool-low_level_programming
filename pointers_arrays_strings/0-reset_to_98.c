40e002aac7b23753debaa1743164bae7bcce1b1b
fatima9821 <bahfatoumat9821@gmail.com>
2023-10-23 11:47:14 -0700

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - take a pointer to an int and updates
+ *@n: pointeur vers un entier dont la valeur doit etre reinitialisee
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
