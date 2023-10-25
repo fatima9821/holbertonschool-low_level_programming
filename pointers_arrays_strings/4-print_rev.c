32a8ccf0105c6878c18c9c62d168471550aa0f31
fatima9821 <bahfatoumat9821@gmail.com>
2023-10-25 04:25:52 -0700

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * print_rev -  imprime en reverse
+ *@s: string
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
