09f2e870f3d33ff75d959bc2ca9e6a702b32e7e6
fatima9821 <bahfatoumat9821@gmail.com>
2023-10-27 03:44:21 -0700

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *puts_half - prints half of a string
+ *@str: string
+ *return:  second half of the string
+ */
+void puts_half(char *str)
+{
+	int len = strlen(str);
+
+	for (len /= 2; str[len] != '\0'; len++)
+	{
+	putchar(str[len]);
+	}
+	putchar('\n');
+
+}
