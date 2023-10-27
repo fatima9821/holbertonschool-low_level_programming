8d6d425f7a2ebbec73df347c1e9d4145da1dcee0
fatima9821 <bahfatoumat9821@gmail.com>
2023-10-27 02:01:01 -0700

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts2 - prints every other character of a string
+ *@str:string in question
+ *return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+	if (i % 2 == 0)
+	{
+	putchar (str[i]);
+	}
+	i++;
+	}
+	putchar('\n');
+}
