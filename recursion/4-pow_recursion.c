4248ee501e46882292b4ae4da121dadc9ddc3465
fatima9821 <bahfatoumat9821@gmail.com>
2023-11-03 08:42:57 -0700

    4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
  * _pow_recursion -  returns the value of x raised to the power of y
  *@y: int(power)
  *@x: int
- *return: result of x power y
+ *Return: result of x power y
  */
 int _pow_recursion(int x, int y)
 {
