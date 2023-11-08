d5feb79ca994986aa2c8b76933c83679be5c324e
fatima9821 <bahfatoumat9821@gmail.com>
2023-11-08 03:04:02 -0800

    solve problem git lock

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,44 +1,6 @@
-#include "main.h"void simple_print_buffer(char *buffer, unsigned int size)
-{
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *buffer;
-
-    buffer = create_array(98, 'H');
-    if  (buffer == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    simple_print_buffer(buffer, 98);
-    free(buffer);
-    return (0);
-}
+#include "main.h"
 #include <stdlib.h>
+
 /**
  *create_array -  a function that creates an array of chars
  *@size: size of array
