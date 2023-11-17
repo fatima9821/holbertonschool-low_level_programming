ce27fc51bececa62262d34d42b40c27dadb37163
fatima9821 <bahfatoumat9821@gmail.com>
2023-11-17 11:30:12 -0800

    variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,19 @@
 #ifndef _VARIADIC_H_
 #define _VARIADIC_H_
 #include <stdarg.h>
+#include <stdio.h>
+
+/**
+ *struct print - struct print
+ *@type: type to print
+ *@f: the function associated
+ */
+
+typedef struct print
+{
+	char type;
+	void (*f)(va_list args);
+} print_f;
 
 int sum_them_all(const unsigned int n, ...);
 
