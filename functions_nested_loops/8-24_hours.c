8a36719a73eb50be7a19e7320f67dbbb070b3282
fatima9821 <bahfatoumat9821@gmail.com>
2023-11-19 13:07:33 -0800

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ * Return:void
+ */
+void jack_bauer(void)
+{
+	int h = 0, min = 0;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
