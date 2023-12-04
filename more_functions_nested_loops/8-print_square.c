b34104d3913b3fa0dba70450cf28cdcc83f7c8fb
fatima9821 <bahfatoumat9821@gmail.com>
2023-12-04 03:37:11 -0800

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints a squareusing the character #
+ * @size: the size of the square
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int hgt;
+	int wid;
+
+	if (size > 0)
+	{
+		for (hgt = 0; hgt < size; hgt++)
+		{
+			for (wid = 0; wid < size; wid++)
+				_putchar('#');
+
+			if (hgt == size - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
